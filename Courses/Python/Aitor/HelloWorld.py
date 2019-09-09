416bf2e9af71bca5f351c6696ebc084c54e25ada
AitorTeran <35167655+AitorTeran@users.noreply.github.com>
2019-09-09 16:26:13 +0200

    Python intro course

--- Courses/Python/Aitor/HelloWorld.py
+++ Courses/Python/Aitor/HelloWorld.py
@@ -1,2 +1,10 @@
-who = "Cristina"
-print("Hello " + who)
+class Person:
+  def __init__(mysillyobject, name, age):
+    mysillyobject.name = name
+    mysillyobject.age = age
+
+  def myfunc(abc):
+    print("Hello my name is " + abc.name)
+
+p1 = Person("John", 36)
+p1.myfunc()
