d4755e274e3bcda8c6f469e2f4b900043cd87619
croche18 <croche18@student.aau.dk>
2019-10-02 10:33:47 +0200

    mpc

---
+++ Courses/Python/Cristina/Prueba 1.py
@@ -0,0 +1,22 @@
+print("hello world")
+
+#indenting
+if 5>2:
+    print("Five is greater than two!")
+
+#random
+
+
+#how to print string and numbers together
+quantity = 3
+itemno = 567
+price = 49.95
+myorder = "I want {} pieces of item {} for {} dollars."
+print(myorder.format(quantity, itemno, price))
+
+#change item value
+thislist = ["apple", "banana", "cherry"]
+thislist[1] = "blackcurrant"
+print(thislist)
+
+import pandas
