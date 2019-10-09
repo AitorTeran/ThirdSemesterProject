0cdae0bcb508b39591d2dac7adf724781edcfc15
PereIzquierdo <54941457+PereIzquierdo@users.noreply.github.com>
2019-10-09 13:38:10 +0200

    Update keras_titanic.py

--- Courses/Python/Pere/Keras_Simulink/keras_titanic.py
+++ Courses/Python/Pere/Keras_Simulink/keras_titanic.py
@@ -30,4 +30,25 @@ model.compile(optimizer='adam',
               loss='binary_crossentropy',
               metrics=['accuracy'])
 
-history = model.fit(x = features, y = labels, batch_size = 10, steps_per_epoch = 100, validation_split = 0.2, epochs = 100) #fit(x=None, y=None, batch_size=None, epochs=1, verbose=1, callbacks=None, validation_split=0.0, validation_data=None, shuffle=True, class_weight=None, sample_weight=None, initial_epoch=0, steps_per_epoch=None, validation_steps=None, validation_freq=1, max_queue_size=10, workers=1, use_multiprocessing=False)
+callbacks = keras.callbacks.TensorBoard(log_dir = ".\Tensorboard_logs")
+
+history = model.fit(
+        x = features,
+        y = labels,
+        batch_size = 10,
+        steps_per_epoch = 100,
+        validation_split = 0.2,
+        epochs = 100,
+        callbacks = [callbacks]) #fit(x=None, y=None, batch_size=None, epochs=1, verbose=1, callbacks=None, validation_split=0.0, validation_data=None, shuffle=True, class_weight=None, sample_weight=None, initial_epoch=0, steps_per_epoch=None, validation_steps=None, validation_freq=1, max_queue_size=10, workers=1, use_multiprocessing=False)
+
+model.save_weights(".\weights.h5", save_format = "h5")
+# Run Tensorboard by navigating to the project folder in CMD and running the command >tensorboard --logdir ".\Tensorboard_logs". Open browser and go to localhost:6006
+
+#%% Plots
+
+tf.keras.utils.plot_model(
+    model,  
+    to_file='model.png',
+    show_shapes=True,
+    show_layer_names=True
+) # Probably needs to have pydot and graphviz installed. Use conda for both packages. Graphviz also needs to have its .msi installed and the dot.exe file added to system path. Refer to google
