c4e9ca43afd62ba4a2296f5862cb96335ada535e
PereIzquierdo <54941457+PereIzquierdo@users.noreply.github.com>
2019-10-20 18:10:57 +0200

    Update imitator.py

--- MPC/Imitator_v1/imitator.py
+++ MPC/Imitator_v1/imitator.py
@@ -1,6 +1,8 @@
 import scipy.io
 import numpy as np
 import pandas as pd
+import tensorflow as tf
+from tensorflow import keras
 
 mat = scipy.io.loadmat("./dataANNcrd.mat")
 
@@ -11,11 +13,48 @@ columns = list(mat.keys())
 columns.remove('__header__')
 columns.remove('__globals__')
 columns.remove('__version__')
-columns.remove('x_opt_old1')
 columns.remove('x_opt')
 columns.remove('vector')
 
-df = pd.DataFrame(np.concatenate([mat[c] for c in columns], axis=1), columns=columns)
-df['state'] = list(result[1]+1)
-df = df.sample(frac = 1)
+train = pd.DataFrame(np.concatenate([mat[c] for c in columns], axis=1), columns=columns)
+train['state'] = list(result[1]+1)
 
+labels = []
+for x in range(7):
+    train['state_' + str(x+1)] = vector[x,:]
+    labels.append('state_' + str(x+1))
+
+
+    
+features = ['R1',
+            'if_alpha1',
+            'if_beta1',
+            'vc_alpha1',
+            'vc_beta1',
+            'vref_alpha1',
+            'vref_beta1',
+            'x_opt_old1']
+
+model = tf.keras.Sequential()
+model.add(tf.keras.layers.Dense(8, activation='relu'))
+model.add(tf.keras.layers.Dense(15, activation='relu'))
+model.add(tf.keras.layers.Dense(7, activation='softmax')) 
+
+model.compile(optimizer='adam',
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+
+callbacks = keras.callbacks.TensorBoard(log_dir = ".\Tensorboard_logs")
+
+train = train.sample(frac = 1)
+
+history = model.fit(
+        x = train[features].to_numpy(),
+        y = train[labels].to_numpy(),
+        batch_size = 10000,
+        steps_per_epoch = 50,
+        validation_split = 0.2,
+        epochs = 10,
+        callbacks = [callbacks])
+
+model.save('./savedmodel.h5')
