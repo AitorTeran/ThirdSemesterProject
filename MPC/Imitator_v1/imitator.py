b005c0f3857c7f4c8862a961db192fb4876d3fcc
PereIzquierdo <54941457+PereIzquierdo@users.noreply.github.com>
2019-10-23 12:39:59 +0200

    Añadir datos de validación

--- MPC/Imitator_v1/imitator.py
+++ MPC/Imitator_v1/imitator.py
@@ -5,9 +5,13 @@ import tensorflow as tf
 from tensorflow import keras
 
 mat = scipy.io.loadmat("./dataANNcrd.mat")
+valmat = scipy.io.loadmat("./Validationdata.mat")
 
 vector = mat['vector']
+valvector = valmat['vector']
+
 result = np.where(vector.T==1) # numpy .T -> trasponer
+valresult = np.where(valvector.T==1)
 
 columns = list(mat.keys())
 columns.remove('__header__')
@@ -19,13 +23,15 @@ columns.remove('vector')
 train = pd.DataFrame(np.concatenate([mat[c] for c in columns], axis=1), columns=columns)
 train['state'] = list(result[1]+1)
 
+val = pd.DataFrame(np.concatenate([valmat[c] for c in columns], axis=1), columns=columns)
+val['state'] = list(valresult[1]+1)
+
 labels = []
 for x in range(7):
     train['state_' + str(x+1)] = vector[x,:]
+    val['state_' + str(x+1)] = valvector[x,:]
     labels.append('state_' + str(x+1))
-
-
-    
+     
 features = ['R1',
             'if_alpha1',
             'if_beta1',
@@ -36,22 +42,23 @@ features = ['R1',
             'x_opt_old1']
 
 model = tf.keras.Sequential()
-model.add(tf.keras.layers.Dense(15, activation='sigmoid', input_dim = 8))
+model.add(tf.keras.layers.Dense(15, activation='relu', input_dim = 8))
 model.add(tf.keras.layers.Dense(7, activation='softmax')) 
 
 model.compile(optimizer='adam',
               loss='categorical_crossentropy',
               metrics=['accuracy'])
 
-callbacks = keras.callbacks.TensorBoard(log_dir = ".\Tensorboard_logs")
+callbacks = keras.callbacks.TensorBoard(log_dir = "./Tensorboard_logs")
 
 train = train.sample(frac = 1)
 
 history = model.fit(
         x = train[features].to_numpy(),
         y = train[labels].to_numpy(),
-        batch_size = 100,
-        validation_split = 0.1,
+        validation_data = (val[features].to_numpy(), val[labels].to_numpy()),
+        batch_size = 50,
         epochs = 10)
+#callbacks = [callbacks]
 
-model.save('./savedmodel.h5')
+model.save('./model_tanh_1.h5')
