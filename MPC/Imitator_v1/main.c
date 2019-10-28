c2db56b3fe5327acd19ee6b9232b5cb3200a0e31
AitorTeran <35167655+AitorTeran@users.noreply.github.com>
2019-10-28 15:39:55 +0100

    Bib y main.c created

---
+++ MPC/Imitator_v1/main.c
@@ -0,0 +1,26 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+float relu(float x)
+{
+    float y = x;
+    if (x < 0) y = 0;
+    return y;
+}
+
+float softmax(float x)
+{
+    
+}
+
+void main()
+{
+    
+}
